75156f8f2c71e67d5b76a73ee357adaf5cfa2ad7
ttoponce <tyler.toponce@gmail.com>
2017-06-21 19:33:11 -0600

    assignment 4.2

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include '../dbConnect.php'; // use include instead of require
+include '../mySQL.php';
+
+$myDBconn = fConnectToDatabase();
+
+fInsertToDatabase($myDBconn);
+fDeleteFromDatabase($myDBconn);
+fListFromDatabase($myDBconn);
+
+fInsertToDatabaseActors($myDBconn);
+fDeleteFromDatabaseActors($myDBconn);
+fListFromDatabaseActors($myBDconn);
+//print_r($myDBconn); // prints that $myDB is a PDO Object to the browser page
+
+  echo '<html>
+          <head>
+            <title>CS3620: Assignment 4</title>
+          </head>
+          <body>
+            <div>ALL DONE!</div>
+          </body>
+        </html>';
